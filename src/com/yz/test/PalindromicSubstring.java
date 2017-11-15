265f77eb6089f7880dd2e65599509e7c9a4dc604
dbdj1201 <15957121194@163.com>
2017-11-15 10:39:52 +0800

    by dbdj1201 lc palindromic substring terrible method

--- src/com/yz/test/PalindromicSubstring.java
+++ src/com/yz/test/PalindromicSubstring.java
@@ -1,5 +1,13 @@
 package com.yz.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class PalindromicSubstring {
 
 	public static String findLongestPalindromicSubstring(String s) {
@@ -8,16 +16,47 @@ public class PalindromicSubstring {
 		// int end = 0;
 
 		String result = "";
+		Map<Integer, String> map = new HashMap<>();
 
-		if (len % 2 != 0) {
-			
+		for (int i = 0; i < len; i++) {
+			for (int j = i + 1; j < len; j++) {
+				String string = s.substring(i, j + 1);
+				System.out.println("substrings: " + string);
+				if (isPalindromic(string)) {
+					map.put(string.length(), string);
+				}
+			}
 		}
 
+		Set<Integer> set = map.keySet();
+		for (Integer integer : set) {
+			System.out.println(integer);
+		}
+
+		System.out.println(map.get(3));
+		int maxNum = Collections.max(set);
+
+		result = map.get(maxNum);
 		return result;
 	}
 
+	public static boolean isPalindromic(String string) {
+		if (string.length() < 3) {
+			return false;
+		}
+
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) == string.charAt(string.length() - i - 1)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	public static void main(String[] args) {
-		PalindromicSubstring.findLongestPalindromicSubstring("abcb");
+		String string = PalindromicSubstring.findLongestPalindromicSubstring("abcb");
+		System.out.println(string);
 
 	}
 
